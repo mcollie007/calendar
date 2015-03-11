b01f774139dd3f0732444cd791543a02203a238b
Michael Collie-Acasio <mcollie_fhc@hotmail.com>
2015-03-11 02:15:16 -0400

    corrected sessions controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,7 +4,7 @@ class SessionsController < ApplicationController
 	end
 
 	def create
-		@user = User.find_by(:email session_params[:email])
+		@user = User.find_by(email: session_params[:email])
 		if @user && @user.authenticate(session_params[:password])
 			session[:user_id] = @user.id 
 			redirect_to days_path
@@ -21,6 +21,6 @@ class SessionsController < ApplicationController
 	private
 
 	def session_params
-		params.require(:sessions).permit(:email, :password)
+		params.require(:session).permit(:email, :password)
 	end
 end
